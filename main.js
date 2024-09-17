1fcbaba3c9b2c955f8b6daaec9e806d61df4fa24
dreakfall <168038388+dreakfall@users.noreply.github.com>
2024-09-17 10:57:42 -0300

    Commit Inicial

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    console.log(document.querySelector('header-button'));
+    console.log($('#botao-cancelar'));
+
+    $('header button').click(function () {
+        $('form').slideDown();
+    });
+
+    $('#botao-cancelar').click(function () {
+        $('form').slideUp();
+    });
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style = "display: none" />');
+        $(`<img src="${enderecoDaNovaImagem}" alt="Nova Imagem" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title= "Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $(`#endereco-nova-imagem`).val('');
+    });
+});
